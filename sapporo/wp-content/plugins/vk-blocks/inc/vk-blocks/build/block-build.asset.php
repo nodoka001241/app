574888797014d65b51c4fb3050cdd35d79aea742
Nodoka <nsam_s.17@icloud.com>
2024-02-28 20:26:58 +0900

    s

--- sapporo/wp-content/plugins/vk-blocks/inc/vk-blocks/build/block-build.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-polyfill', 'wp-primitives', 'wp-rich-text', 'wp-server-side-render'), 'version' => '2488abf932e989c4b705');
