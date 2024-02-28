b65dc21ab1c8570c4eab3cfd7f081430e41e717c
Nodoka <nsam_s.17@icloud.com>
2024-02-28 20:27:28 +0900

    d

---
+++ public/wp-content/plugins/event-tickets/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'the-events-calendar/event-tickets',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3130645d34ec3c0b1bc90263b3632252f020aa7a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'the-events-calendar/event-tickets' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3130645d34ec3c0b1bc90263b3632252f020aa7a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
